bf65ceaeff2fada593d72a015cd6d6bf46d683ee
Moxitron <70322560+moxitron@users.noreply.github.com>
2023-01-12 20:02:02 +0330

    debug nn.py

--- nn.py
+++ nn.py
@@ -1,5 +1,4 @@
 import numpy as np
-from math import exp
 
 
 class NeuralNetwork():
@@ -8,21 +7,22 @@ class NeuralNetwork():
 
         # sizes example: [4, 10, 2]
 
-        self.baises = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(x, y) / np.sqrt(x) 
                         for (x, y) in zip(sizes[1:], sizes[:-1]) ] 
 
         self.activation = np.vectorize(self.activation) 
 
     def activation(self, x):
-        x[x>0] = 1 / (1 + exp(-x))
-        x[x<0] = exp(x) / (1 + exp(x))
+        x[x>0] = 1 / (1 + np.exp(-x[x>0]))
+        x[x<0] = np.exp(x[x<0]) / (1 + np.exp(x[x<0]))
         return x 
 
     def forward(self, x):
         
         # x example: np.array([[0.1], [0.2], [0.3]])
+        a = x.reshape(-1, 1) 
         for w, b in zip(self.weights, self.biases) :
-            a = self.activation(w.T @ x + b)
+            a = (np.matmul(w, a) + b)
 
-        return a 
+        return float(a)
