ed41cf20c1cc3af0579febc4eb4346e56c34682a
Moxitron <70322560+moxitron@users.noreply.github.com>
2023-01-12 19:03:22 +0330

    add network implementation

--- nn.py
+++ nn.py
@@ -1,21 +1,28 @@
 import numpy as np
+from math import exp
 
 
 class NeuralNetwork():
 
-    def __init__(self, layer_sizes):
+    def __init__(self, sizes):
 
-        # TODO
-        # layer_sizes example: [4, 10, 2]
-        pass
+        # sizes example: [4, 10, 2]
+
+        self.baises = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(x, y) / np.sqrt(x) 
+                        for (x, y) in zip(sizes[1:], sizes[:-1]) ] 
+
+        self.activation = np.vectorize(self.activation) 
 
     def activation(self, x):
-        
-        # TODO
-        return x
+        x[x>0] = 1 / (1 + exp(-x))
+        x[x<0] = exp(x) / (1 + exp(x))
+        return x 
 
     def forward(self, x):
         
-        # TODO
         # x example: np.array([[0.1], [0.2], [0.3]])
-        pass
+        for w, b in zip(self.weights, self.biases) :
+            a = self.activation(w.T @ x + b)
+
+        return a 
