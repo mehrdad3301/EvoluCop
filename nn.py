22658f04337dcc71e009ae0f5fa82a1a72003584
Moxitron <70322560+moxitron@users.noreply.github.com>
2023-01-16 23:37:31 +0330

    fixing bug in nn.py

--- nn.py
+++ nn.py
@@ -5,24 +5,18 @@ class NeuralNetwork():
 
     def __init__(self, sizes):
 
-        # sizes example: [4, 10, 2]
-
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
-        self.weights = [np.random.randn(x, y) / np.sqrt(x) 
+        self.weights = [np.random.randn(x, y) 
                         for (x, y) in zip(sizes[1:], sizes[:-1]) ] 
 
-        self.activation = np.vectorize(self.activation) 
-
     def activation(self, x):
-        x[x>0] = 1 / (1 + np.exp(-x[x>0]))
-        x[x<0] = np.exp(x[x<0]) / (1 + np.exp(x[x<0]))
-        return x 
+
+        return 1 / ( 1 + np.exp(-x) )
 
     def forward(self, x):
-        
-        # x example: np.array([[0.1], [0.2], [0.3]])
+
         a = x 
         for w, b in zip(self.weights, self.biases) :
-            a = (np.matmul(w, a) + b)
+            a = self.activation(np.matmul(w, a) + b)
 
         return float(a)
