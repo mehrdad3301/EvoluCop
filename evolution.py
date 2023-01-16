4daa4e07b1aec12a2517118277add4ccecb88f5e
Moxitron <70322560+moxitron@users.noreply.github.com>
2023-01-17 00:06:07 +0330

    refactor: moving selection logic from evolution

--- evolution.py
+++ evolution.py
@@ -3,6 +3,7 @@ import numpy as np
 from copy import deepcopy
 
 from recombination import crossover, mutate
+from selection import roulette_wheel, q_tournament
 
 class Evolution():
 
@@ -13,44 +14,17 @@ class Evolution():
         for i, p in enumerate(players):
             p.fitness = delta_xs[i]
 
-
     def generate_new_population(self, num_players, prev_players=None):
 
         if prev_players is None:
             return [Player(self.mode) for _ in range(num_players)]
-
         else:
             players_copy = deepcopy(prev_players)
-            parents = self.roulette_wheel(players_copy, num_players)
+            parents = q_tournament(players_copy, num_players, 10)
             children = crossover(parents)
+            children = parents
             children = [mutate(c, 0.5, 0.7, 1) for c in children]
             return children
 
     def next_population_selection(self, players, num_players):
-        return self.top_k(players, num_players)
-
-    def top_k(self, players, k):
-        return sorted(players)[-k:]
-
-    def rank_based_selection(self, players, k):
-        players.sort()
-        p = np.arange(len(players)) ** 2
-        p = p / p.sum()
-        return deepcopy(np.random.choice(players, size=k, p=p))
-
-    def q_tournament(self, players, num_players, q):
-        new_players = []
-        for _ in range(num_players):
-            tournoment = np.random.choice(players, q)
-            new_players.append(deepcopy(max(tournoment)))
-        return new_players
-
-    def roulette_wheel(self, players, num_players):
-
-        fitness = [p.fitness for p in players]
-        print(sorted(fitness))
-        fitness = fitness / np.sum(fitness)
-        return list(np.random.choice(players, size=num_players, 
-                                p=fitness, replace=False))
-
-    
+        return roulette_wheel(players, num_players, False)
---
+++ selection.py
@@ -0,0 +1,27 @@
+import numpy as np 
+from copy import deepcopy
+
+def top_k(players, k):
+    return sorted(players)[-k:]
+
+def rank_based_selection(players, k):
+    players.sort()
+    p = np.arange(len(players)) ** 2
+    p = p / p.sum()
+    return deepcopy(np.random.choice(players, size=k, p=p))
+
+def q_tournament(players, num_players, q):
+    new_players = []
+    for _ in range(num_players):
+        tournoment = np.random.choice(players, q)
+        new_players.append(deepcopy(max(tournoment)))
+    return new_players
+
+def roulette_wheel(players, num_players, replace=True):
+
+    fitness = [p.fitness for p in players]
+    fitness = fitness / np.sum(fitness)
+    return deepcopy(list(np.random.choice(players, 
+    size=num_players, p=fitness, replace=replace)))
+
+    
